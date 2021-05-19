10560b0327e714837780f289c2cf84cea7c4a615
marcoplaitano <marcoplaitano23@gmail.com>
2021-05-19 19:50:39 +0200

    using pulse width modulation to control motor

--- sources/sunshield.py
+++ sources/sunshield.py
@@ -1,36 +1,39 @@
-from servo import servo
+import pwm
 
 
-COVER_DEGREE = 90
-UNCOVER_DEGREE = 0
+PERIOD = 20000
+COVER_PW = 1700
+UNCOVER_PW = 700
 
 
 class Sunshield():
     def __init__(self, pin, client):
-        self._motor = servo.Servo(pin)
+        self._pin = pin
         self._client = client
+        self._position = UNCOVER_PW
+        pwm.write(self._pin, PERIOD, UNCOVER_PW, MICROS)
 
 
-    def init(self):
-        self._motor.attach()
-        self._motor.moveToDegree(0)
+    def _move_to(self, pulse):
+        pwm.write(self._pin, PERIOD, pulse, MICROS)
+        self._position = pulse
 
 
     def is_covering(self):
-        return self._motor.getCurrentDegree() != UNCOVER_DEGREE
+        return self._position != UNCOVER_PW
 
 
     def cover(self):
         if self.is_covering():
             return
-        self._motor.moveToDegree(COVER_DEGREE)
+        self._move_to(COVER_PW)
         self._client.publish("iot-marco/data/sunshield", "covering")
 
 
     def uncover(self):
         if not self.is_covering():
             return
-        self._motor.moveToDegree(UNCOVER_DEGREE)
+        self._move_to(UNCOVER_PW)
         self._client.publish("iot-marco/data/sunshield", "not covering")
 
 
@@ -41,3 +44,7 @@ class Sunshield():
     def control(self, command):
         if command == "get-state":
             self._client.publish("iot-marco/data/sunshield", self.state())
+        elif command == "cover":
+            self.cover()
+        elif command == "uncover":
+            self.uncover()
